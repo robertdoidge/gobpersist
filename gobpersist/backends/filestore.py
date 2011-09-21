a6a3f526e4e06fbb47a755f4d320cd7e5eb328ba
murray foster <murray.foster@accellion.com>
2011-09-21 13:02:14 -0700

    Recached the filesystem.  Modified storage engine so it now better mimics inheritance structure found in acellion/api/apiobj.py. Changed filename diskcache.py to filestore.py.  It now unifies both permanent and temporary storage to interoperate. Changed filename tokyofilestore.py to tokyocabinet.py.  Also split main class TokyoDiskStore into TokyoCabinetBackend and its inheritor TokyoCabinetCache that more closely resembles the way memached.MemcachedCache's inheritance scheme.

--- gobpersist/backends/filestore.py
+++ gobpersist/backends/filestore.py
@@ -2,15 +2,15 @@ from __future__ import absolute_import
 
 from gobpersist import session
 from accellion import gobphpapi
+from wsgiref.util import FileWrapper
 
-class CachingStorageEngine(session.StorageEngine):
+class FileStorageBackend(session.StorageEngine):
 
     def __init__(self, storage_engine=None, cache=None):
         """storage engine backends"""
         self.storage_engine = storage_engine
-        self.cache = cache(storage_engine=self.storage_engine)
-        print 'DONE'
-
+        self.cache = cache
+        
     def upload(self, gob, fp):
         """uploads a file to the storage engine"""
 
@@ -20,12 +20,17 @@ class CachingStorageEngine(session.StorageEngine):
 
     def download(self, gob):
         """attempts to download file from cache, otherwise moves on to the storage engine"""
-        gob2, iterable = self.cache.download(gob)
-
-        return gob2, iterable
+        (gob2, iterable) = self.cache.download(gob)
+        if iterable == -1:
+            (gob2, iterable) = self.storage_engine.download(gob)
+            self.cache.upload(gob, iterable)
+            (gob2, iterable) = self.cache.download(gob)
+      
+        return (gob2, iterable)
 
     def upload_iter(self, gob, iterable):
         """same as upload(), but includes a wrapper for an iterable"""
-        gob2 = self.storage_engine.uplaod_iter(gob, iterable)
+
+        gob2 = self.storage_engine.upload_iter(gob, iterable)
         
         return gob2
--- gobpersist/backends/tokyocabinet.py
+++ gobpersist/backends/tokyocabinet.py
@@ -3,35 +3,28 @@ from __future__ import absolute_import
 from gobpersist.backends import minifs
 from gobpersist import session
 
-from accellion import gobphpapi
-
 import os,sys
 import pyrant
 import uuid
 
 import jsonpickle
 
-# diskcache.py
+# tokyocabinet.py
 # utilizes pyrant, tokyotyrant, tokyocabinet, and minifs.py to
-# implement backend storage and caching for user files
+# implement backend storage caching for user files
 
 KEYFIELD = 'file_id'
 """the File field we want to use to generate keys, accellion/schema.py"""
 
-class TokyoDiskStore(session.StorageEngine):
+class TokyoCabinetBackend(session.StorageEngine):
     """TokyoTyrant client and TokyoCabinet dbm for storage control"""
-    def __init__(self, server='127.0.0.1', port=1978, storage_engine=None, cachepath='accellion_cache', cachesize=2000):
-
-        self.storage_engine = storage_engine
-        """permanent storage backend to be used with this cache"""
-
+    def __init__(self, server='127.0.0.1', port=1978, cachepath='accellion_cache', cachesize=2000):
         self.tt = pyrant.Tyrant(server, port)
-        """tokyo cabinet client"""
 
+        """tokyo cabinet client"""
         self.filerecord_cache = []
         self.sizerecord_cache = {}
         self.cache_spaceleft = 0
-        """record-keeping variables"""
 
         self.filecache_directory = cachepath
         """directories for storage"""
@@ -39,6 +32,8 @@ class TokyoDiskStore(session.StorageEngine):
         self.filecache_size = cachesize
         """sizes of storage containers"""
 
+        self.cache_storage = minifs.MRUGobPreserve(self.filecache_directory, self.filecache_size)
+        """temporary disk storage"""        
 
         try:
             self.filerecord_cache = jsonpickle.decode(self.tt['filerec_cache'])
@@ -48,10 +43,7 @@ class TokyoDiskStore(session.StorageEngine):
             print 'this is the first time setting up this storage engine.'
             self.clean_directory()
             self.filerecord_cache = -1
-            
-        self.cache_storage = minifs.MRUGobPreserve(self.filecache_directory, self.filecache_size)
-        """temporary disk storage"""        
-        
+                        
         if self.filerecord_cache != -1:
             self.cache_storage.recordregistry = self.filerecord_cache
             self.cache_storage.sizeregistry = self.sizerecord_cache
@@ -62,33 +54,9 @@ class TokyoDiskStore(session.StorageEngine):
             path = os.path.join(self.filecache_directory, elem)
             try:
                 os.unlink(path)
-
             except Exception, e:
                 print e
 
-    def store_permanent_records(self):
-        """in case of a system halt, recall saved file information"""
-        self.tt['filerec_cache'] = jsonpickle.encode(self.cache_storage.recordregistry)
-        self.tt['sizerec_cache'] = jsonpickle.encode(self.cache_storage.sizeregistry)
-        self.tt['cachespace'] = jsonpickle.encode(self.cache_storage.partremsize)
-
-    def download(self, gob):
-        """call on the storage to retrieve data"""
-        fp = None
-        
-        fp = self.cache_storage.get(getattr(gob, KEYFIELD).value)
-        if fp != -1:
-            self.store_permanent_records()
-            return (gob, fp)
-        gob2, iterable = self.storage_engine.download(gob)
-        deleted = self.cache_storage.add(getattr(gob, KEYFIELD).value, iterable, getattr(gob, 'size').value)
-        
-        for elem in deleted:
-            del self.tt[elem]
-
-        self.store_permanent_records()
-        return (gob, fp)
-    
     def search(self, gob):
         """search tokyocabinet for a gob's file handle"""
         try:
@@ -96,3 +64,40 @@ class TokyoDiskStore(session.StorageEngine):
             return 0
         except KeyError:
             return -1
+
+class TokyoCabinetCache(TokyoCabinetBackend, session.StorageEngine):
+    
+    def __init__(self, server='127.0.0.1', port=1978, cachepath='accellion_cache', cachesize=2000):    
+        super(TokyoCabinetCache, self).__init__(server, port, cachepath, cachesize)
+        
+    def remove_deleted(self, deleted):
+        for elem in deleted:
+            del self.tt[elem]
+    
+    def store_permanent_records(self):
+        """in case of a system halt, recall saved file information"""
+        self.tt['filerec_cache'] = jsonpickle.encode(self.cache_storage.recordregistry)
+        self.tt['sizerec_cache'] = jsonpickle.encode(self.cache_storage.sizeregistry)
+        self.tt['cachespace'] = jsonpickle.encode(self.cache_storage.partremsize)
+    
+    def download(self, gob):
+        """call on the storage to retrieve data"""
+        iterable = None
+        
+        iterable = self.cache_storage.get(getattr(gob, KEYFIELD).value)
+        
+        if iterable != -1:
+            self.store_permanent_records()
+            return (gob, iterable)
+
+        return(gob, -1)
+    
+    def upload(self, gob, iterable):
+        
+        deleted = self.cache_storage.add(getattr(gob, KEYFIELD).value, iterable, getattr(gob, 'size').value)
+        self.remove_deleted(deleted)
+
+        self.store_permanent_records()
+        return gob
+
+     
